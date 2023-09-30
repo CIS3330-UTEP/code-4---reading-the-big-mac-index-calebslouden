552de6e7f5105069f45e0caf1ac6cf49e53ae88b
calebslouden <cslouden@miners.utep.edu>
2023-09-29 22:00:44 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -3,11 +3,9 @@ import pandas as pd
 
 big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv(big_mac_file)
-df['iso_a3'] = df['iso_a3'].str.lower()
 
 def get_big_mac_price_by_year(year, country_code):
-    year = 2006
-    country_code = "can"
+    country_code = country_code.upper()
     query_text = f"(date >= '{year}-01-01' and date <= '{year}-12-31' and iso_a3 == '{country_code}')"
     df_result = df.query(query_text)
     mean_dollar_value = df_result['dollar_price'].mean()
@@ -15,7 +13,7 @@ def get_big_mac_price_by_year(year, country_code):
     return rounded_mean
 
 def get_big_mac_price_by_country(country_code):
-    country_code = "jpn"
+    country_code = country_code.upper()
     query_text = f"(iso_a3 == '{country_code}')"
     df_result = df.query(query_text)
     mean_dollar_value = df_result['dollar_price'].mean()
@@ -23,27 +21,25 @@ def get_big_mac_price_by_country(country_code):
     return rounded_mean
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    year = 2016
-    country_code = "ven"
     query_text = f"(date >= '{year}-01-01' and date <= '{year}-12-31')"
     df_result = df.query(query_text)
-    min_value = df_result['dollar_price'].min()
-    rounded_min_value = round(min_value,2)
+    index_min_value = df_result['dollar_price'].idxmin()
+    min_value = df_result.loc[index_min_value]
+    rounded_min_value = f"{min_value['name']}({min_value['iso_a3']}): {round(min_value['dollar_price'],2)}"
     return rounded_min_value
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    year = 2006
-    country_code = "nor"
     query_text = f"(date >= '{year}-01-01' and date <= '{year}-12-31')"
     df_result = df.query(query_text)
-    max_value = df_result['dollar_price'].max()
-    rounded_max_value = round(max_value,2)
+    index_max_value = df_result['dollar_price'].idxmax()
+    max_value = df_result.loc[index_max_value]
+    rounded_max_value = f"{max_value['name']}({max_value['iso_a3']}): {round(max_value['dollar_price'],2)}"
     return rounded_max_value
 
 if __name__ == "__main__":
     year = 2006
-    country_code = "can"
+    country_code = 'can'
     print(get_big_mac_price_by_year(year, country_code))
     print(get_big_mac_price_by_country(country_code))
-    print (f"Venezuela(VEN): {get_the_cheapest_big_mac_price_by_year(year)}")
-    print (f"Norway(NOR): {get_the_most_expensive_big_mac_price_by_year(year)}")
+    print(get_the_cheapest_big_mac_price_by_year(year))
+    print(get_the_most_expensive_big_mac_price_by_year(year))
